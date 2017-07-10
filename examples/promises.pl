79c4086cb0b4c823f47b2fae44c6124906f7bec6
Brian Fraser <brian.fraser@booking.com>
2017-07-11 00:41:32 +0200

    promises example: collect() does not work as I think it should! >:o

--- examples/promises.pl
+++ examples/promises.pl
@@ -3,29 +3,70 @@ use v5.10.1;
 use strict;
 use warnings;
 
-use Devel::Peek;
-use Data::Dumper;
-
 use blib;
-use Promises qw/collect/;
-use Scalar::Util qw/weaken/;
+use Promises ();
 use MariaDB::NonBlocking::Promises;
 
-sub MariaDB::NonBlocking::Promises::DESTROY { say STDERR "$_[0]: OH HAI"; }
+# methinks collect() has a (doc?) bug
+#perl -MPromises=collect,deferred -MAnyEvent -E 'my ($x, $y) = map deferred, 1..2; my $cv = AnyEvent->condvar; collect($x->promise, $y->promise)->then(sub { say "All finished: @_"; $cv->send; }, sub { say "Error! @_"; $cv->send; }); my @w = ( AnyEvent->timer(after => 1, cb => sub { say "Resolving X"; $x->reject("X") }), AnyEvent->timer(after => 2, cb => sub { say "Resolving Y"; $y->resolve("Y") })); $cv->recv; say "End!"'
+
+# In the same vein as collect(), but will *not* immediately
+# reject the promise if any single one fails.
+# Useful when we want all connections to gracefully stop
+# their processing.
+sub collect_all_resolve_or_reject {
+    my $deferred  = Promises::deferred;
+    my $remaining = @_;
+    my (@results, @errors);
+
+    foreach my $idx ( 0..$#_ ) {
+        $_[$idx]->then(
+            sub {
+                $remaining--;
+                $results[$idx] = \@_;
+                if ( !$remaining ) {
+                    @errors
+                        ? $deferred->reject(@errors)
+                        : $deferred->resolve(@results);
+                }
+            },
+            sub {
+                $remaining--;
+                push @errors, @_;
+                $deferred->reject(@errors) if !$remaining;
+            }
+        )
+    }
+
+    if ( !$remaining && $deferred->is_in_progress ) {
+        @errors
+            ? $deferred->reject(@errors)
+            : $deferred->resolve(@results);
+    }
+
+    return $deferred->promise;
+}
 
 my @queries = (
-    "SELECT CONNECTION_ID(), RAND(6959)",
-    "select coord(), rand(464)",
-    "select 1, rand(464)",
+    [
+        "SELECT CONNECTION_ID(), RAND(6959)",
+        "select doof(), rand(464)",
+        "select 1, rand(464)",
+    ],
+    [
+        "select sleep(3)"
+    ],
+    [
+        "select sleep(4)"
+    ],
 );
-
 
 my @promises;
 my @all_connections;
 my $extras = {}; # Used to cancel all promises in case of an error
-for (1..1) {
-    my $init_connections = [ map MariaDB::NonBlocking::Promises->init, 1..2 ];
-    say join "\n", @$init_connections;
+for (1..3) {
+    # Create 9 connections to mysql (3 promises with 3 connections each)
+    my $init_connections = [ map MariaDB::NonBlocking::Promises->init, 1..1 ];
 
     # Used during error handling to disconnect everything early
     push @all_connections, $init_connections;
@@ -33,17 +74,22 @@ for (1..1) {
     my $handle_errors = sub {
         # If we already handled an error then we have nothing to do
         return if exists $extras->{cancel};
+        # By setting $extras->{cancel}, the other promises will
+        # cancel themselves the next time their callbacks are
+        # tickled by the eventloop.  And we force that to happen
+        # with the disconnect below!
         $extras->{cancel} = "Error in related promise";
 
         # Disconnect should not die -- if it does, then
         # that takes precedence to whatever else happened!
-        $_->disconnect for map @{$_//[]}, @all_connections;
+        $_->disconnect for grep defined,
+                           map @{$_//[]},
+                           @all_connections;
         @all_connections = ();
         # ^ At this point, the connections for this promise will
         # be freed.
 
-        my $e = $_[0];
-        die $e; # rethrow
+        die $_[0]; # rethrow
     };
 
     push @promises, MariaDB::NonBlocking::Promises::connect(
@@ -56,33 +102,32 @@ for (1..1) {
         $extras,
     )->then(
         sub {
-            return if exists $extras->{cancel};
+            return if exists $extras->{cancel}; # Another connection died, stawp
 
             my ($connections) = @_;
 
-            # returns a promise that will be resolved once the
-            # query is run once on each connection
-            my $promise
-                = MariaDB::NonBlocking::Promises::run_multiple_queries(
+            # Return a promise that will be resolved once all the
+            # queries are run; the resolve handler will then get
+            # the per-query results
+            return MariaDB::NonBlocking::Promises::run_multiple_queries(
                     $connections,
-                    [@queries],
+                    pop @queries,
                     $extras,
-                )->catch($handle_errors);
-            return $promise;
+                   );
         },
-        $handle_errors
-    );
+    )->catch($handle_errors);
 }
 
 {
+    # Wait for the promises to be fulfilled.
     use AnyEvent;
     my $cv = AnyEvent->condvar;
-    collect(@promises)->then(
-        sub { say "hello! ", Dumper(\@_); $cv->send },
-        sub { say "error! @_";            $cv->send },
+    collect_all_resolve_or_reject(@promises)->then(
+        sub { @all_connections = (); use Data::Dumper; say Dumper(\@_); $cv->send },
+        sub { say "Error! @_"; $cv->send },
     );
     $cv->recv;
+    @promises = ();
 }
-say "promise wait END";
 
 say "END";
